cf281e1935c322300fc22972698be7fde42d339f
Stepan <nemail.necom@gmail.com>
2021-01-28 01:14:43 +0300

    makes one handleChange

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 
 export default class PostForm extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { title: '', postText: '' };
   }
   // Handlers
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.value);
-    this.setState({value: ''})
+    console.log(this.state);
+    //this.setState({ postText: '' });
   };
   handleChange = (e) => {
-    this.setState({value: e.target.value});
+    this.setState((prev) => ({
+      ...prev,
+      ...{ [e.target.name]: e.target.value },
+    }));
   };
   // JSX
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+        {/* Title Input */}
+        <div className='mb-3'>
+          <label htmlFor='title' className='form-label'>
+            Title
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='title'
+            name='title'
+            onChange={this.handleChange}
+          />
+        </div>
+        {/* Post Input */}
         <div className='mb-3'>
           <label htmlFor='post' className='form-label'>
             Post
@@ -26,11 +42,13 @@ export default class PostForm extends Component {
           <textarea
             className='form-control'
             id='post'
+            name='postText'
             rows='2'
             value={this.state.value}
             onChange={this.handleChange}
           ></textarea>
         </div>
+
         <button type='submit' className='btn btn-primary'>
           Submit
         </button>
