c2b1baa39d0ea21b13fcc64562345d8b3ec84cc1
Stepan <nemail.necom@gmail.com>
2021-01-27 23:36:00 +0300

    Created main container

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import MyPostContainer from './Components/MyPostContainer';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
-import FormikContainer from './Components/FormikContainer';
-import { Route } from 'react-router';
-import { NavLink } from 'react-router-dom';
 
-function App(props) {
+
+
+function App() {
   return (
-    <div>
-      <div className='ui  menu'>
-        <NavLink
-          to='/posts'
-          className='orange item'
-        >
-          Posts
-        </NavLink>
-        <NavLink
-          to='/form'
-          className='orange item'
-        >
-          Form
-        </NavLink>
-      </div>
-      <div className='App'>
-        <Route path='/posts' render={() => <MyPostContainer />} />
-        <Route path='/form' render={() => <FormikContainer />} />
-      </div>
+    <div className='container'>
+      <h2>React</h2>
     </div>
   );
 }
