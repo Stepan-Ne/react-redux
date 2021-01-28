6b721f9432634e7764fc79aebf657a2b64ab7d4d
Stepan <nemail.necom@gmail.com>
2021-01-28 15:51:49 +0300

    thunk - step 1 - actionCreator

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,11 @@
 import postsReducer from './postReducer';
-import {combineReducers, createStore, compose} from 'redux';
+import {combineReducers, createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 
 const rootReducer = combineReducers({
     posts: postsReducer
 })
-const store = createStore(rootReducer, compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store;
